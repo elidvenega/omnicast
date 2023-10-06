456cade8feb469abff013182aea9ddc4a2cb05ad
elidvenega <venegaelid@gmail.com>
2023-10-06 06:49:41 -0700

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../style.css';
+	import '../app.css';
+</script>
+
+<main>
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import MainPage from "../lib/layout/MainPage.svelte";
+	import "../style.css"
+</script>
+
+<MainPage />
